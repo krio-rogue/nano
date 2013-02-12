ddd483f4a6ac67c9e1356507190ce891b6a945f3
krio-rogue <krio.rogue@gmail.com>
2013-02-12 11:00:49 +0200

    Update jquery.nano.js
    allow use function

--- jquery.nano.js
+++ jquery.nano.js
@@ -4,8 +4,10 @@
   $.nano = function(template, data) {
     return template.replace(/\{([\w\.]*)\}/g, function (str, key) {
       var keys = key.split("."), value = data[keys.shift()];
-      $.each(keys, function () { value = value[this]; });
-      return (value === null || value === undefined) ? "" : value;
+      $.each(keys, function () {
+        value = (typeof value[this] == 'function' ? value[this](data) : value[this]);
+      };
+      return (value === null || value === undefined) ? "" : (typeof value == 'function' ? value.apply(data) : value);
     });
   };
 })(jQuery);
